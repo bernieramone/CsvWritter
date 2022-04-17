92cd8439a6298db26753c23bbd8d918a77141d63
bernie_ramone <makio1981@hotmail.com>
2022-04-17 16:23:56 -0500

    Create column structure

--- src/CsvWritter/Program.cs
+++ src/CsvWritter/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CsvWritter;
+
 Console.WriteLine("Hello, World!");
 
 
+var dateColumne = Columns.Date;
+
+Console.WriteLine(dateColumne);
